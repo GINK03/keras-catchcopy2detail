fc8f3e5bb8dcdcb7762e33d35bc5b42b0b5cccb8
Your Name <gim.kobayashi@gmail.com>
2017-06-18 20:42:27 +0900

    u

--- looper.py
+++ looper.py
@@ -2,5 +2,10 @@
 import os
 import sys
 
-for i in range(3000):
-  os.system("CUDA_VISIBLE_DEVICES=1 python3 c2d.py --train --resume")
+if '-1' in sys.argv:
+  for i in range(3000):
+    os.system("CUDA_VISIBLE_DEVICES=1 python3 c2d.py --train --resume")
+
+if '-0' in sys.argv:
+  for i in range(3000):
+    os.system("CUDA_VISIBLE_DEVICES=0 python3 c2d.py --train --resume")
